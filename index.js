a6e2b1354104ad3cd97e7ae5961a8b7cd2aa634c
AlvaroRubioGomez <44999988+AlvaroRubioGomez@users.noreply.github.com>
2021-07-08 23:01:56 +0100

    added basic graphql structure

---
+++ index.js
@@ -0,0 +1,29 @@
+const { graphql, buildSchema } = require('graphql')
+const { makeExecutableSchema } = require('graphql-tools')
+const express = require('express')
+const { graphqlHTTP } = require('express-graphql')
+const { readFileSync } = require('fs')
+const { join } = require('path')
+const resolvers = require('./lib/resolvers')
+
+const app = express()
+const port = process.env.port || 3000
+
+// Define the schema
+const typeDefs = readFileSync(
+    join(__dirname, 'lib', 'schema.graphql'),
+    'utf-8'
+  )
+  const schema = makeExecutableSchema({ typeDefs, resolvers })
+
+// Inject graphqlHTTP in our app
+app.use('/api', graphqlHTTP({
+  schema: schema,
+  rootValue: resolvers,
+  graphiql: true
+}))
+
+// Up our server
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}/api`);
+})
